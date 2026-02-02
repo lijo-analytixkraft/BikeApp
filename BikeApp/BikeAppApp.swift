c54f6b73cfb0287d30e57e0436bd89fcde742df3
llawrance <lijo.lawrence@gopuff.com>
2026-02-02 18:55:10 +0530

    Initial Commit

---
+++ BikeApp/BikeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeAppApp.swift
+//  BikeApp
+//
+//  Created by Lijo Lawrance on 02/02/26.
+//
+
+import SwiftUI
+
+@main
+struct BikeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
