2599fcc394d97df24eb637b162f002592d614f4b
llawrance <lijo.lawrence@gopuff.com>
2026-02-11 12:59:10 +0530

    feat: refine workout UX, track handling, and cadence-speed analytics

--- BikeApp/BikeAppApp.swift
+++ BikeApp/BikeAppApp.swift
@@ -3,26 +3,31 @@ import SwiftUI
 @main
 struct BikeAppApp: App {
     @StateObject private var profileStore: ProfileStore
+    @StateObject private var historyStore: WorkoutHistoryStore
 
     init() {
-        _profileStore = StateObject(wrappedValue: ProfileStore())
+        let profileStore = ProfileStore()
+        let historyStore = WorkoutHistoryStore()
+        _profileStore = StateObject(wrappedValue: profileStore)
+        _historyStore = StateObject(wrappedValue: historyStore)
     }
 
     var body: some Scene {
         WindowGroup {
-            RootView(profileStore: profileStore)
+            RootView(profileStore: profileStore, historyStore: historyStore)
         }
     }
 }
 
 private struct RootView: View {
     @ObservedObject var profileStore: ProfileStore
+    @ObservedObject var historyStore: WorkoutHistoryStore
 
     var body: some View {
         if profileStore.activeProfileId == nil {
             ProfileSelectionView(store: profileStore, selectionRequired: true)
         } else {
-            ContentView(profileStore: profileStore)
+            ContentView(profileStore: profileStore, historyStore: historyStore)
         }
     }
 }
